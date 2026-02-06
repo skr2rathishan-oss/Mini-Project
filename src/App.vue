c7e2dc493d23b59e207f8626427e874f1e0497b0
skr2rathishan-oss <skr2rathishan@gmail.com>
2026-02-06 20:55:21 +0530

    Initial commit with vue app created

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const message = ref("Hello")
+
+function changeMessage() {
+  message.value = "Button Clicked!"
+}
+</script>
+
+<template>
+  <div>
+    <h2>{{ message }}</h2>
+    <button @click="changeMessage">Click Me</button>
+  </div>
+</template>
