662b028836e9b0c7665bba992a6cc703bd84f4e7
skr2rathishan-oss <skr2rathishan@gmail.com>
2026-02-07 23:17:53 +0530

    headebar created for the ecommerce website

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,76 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import Navbar from './components/Navbar.vue'
 
-const message = ref("Hello")
+const isOpen = ref(false)
 
-function changeMessage() {
-  message.value = "Button Clicked!"
+const selectedCurrency = ref('LKR')
+
+const toggleCurrency = () => {
+  isOpen.value = !isOpen.value
+}
+
+const selectCurrency = (cur: string) => {
+  selectedCurrency.value = cur
+  isOpen.value = false
+}
+
+const cartCount = ref(3)
+const categories = ref([])
+const selectedCategory = ref('')
+const isLoggedIn = ref(false)
+const userProfile = ref({ name: '', email: '' })
+
+const handleSearch = (q: string) => {
+  window.alert('Searching for: ' + q)
+}
+
+const handleCartClick = () => {
+  window.alert('Cart clicked')
+}
+
+const handleLogoClick = () => {
+  window.alert('Home clicked')
+}
+
+const onCartClick = () => {
+  handleCartClick()
+}
+
+const onSearch = (q: string) => {
+  handleSearch(q)
+}
+
+const onLogoClick = () => {
+  handleLogoClick()
+}
+
+const onSettingsClick = () => {
+  window.alert('Settings clicked')
+}
+
+const onSignInClick = () => {
+  window.alert('Sign in clicked')
+}
+
+const onLogout = () => {
+  window.alert('Logout clicked')
 }
 </script>
 
 <template>
-  <div>
-    <h2>{{ message }}</h2>
-    <button @click="changeMessage">Click Me</button>
-  </div>
+  <Navbar
+  :cartCount="cartCount"
+  :categories="categories"
+  :selectedCategory="selectedCategory"
+  :isLoggedIn="isLoggedIn"
+  :userProfile="userProfile"
+  @cartClick="onCartClick"
+  @search="onSearch"
+  @logoClick="onLogoClick"
+  @settingsClick="onSettingsClick"
+  @signInClick="onSignInClick"
+  @logout="onLogout"
+/>
+
 </template>
